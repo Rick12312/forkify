e043e11aae9e4313b26b81e6ee78980888a53070
Rick <rick@Ricks-MacBook-Air.local>
2022-07-28 17:22:11 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3df4c481-384b-49fd-9321-918adea5e7df';
+export const MODAL_CLOSE_SEC = 2.5;
