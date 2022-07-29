1ebaf697e515e516b98977157002d2eb18757d4e
Rick <rick@Ricks-MacBook-Air.local>
2022-07-29 15:53:21 +0100

    Fixed dependancy issue with fractional, replaced with Fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from '../../img/icons.svg';
-import { Fraction } from 'fractional';
+var fracty = require('fracty');
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -129,9 +129,7 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ingredient.quantity
-              ? new Fraction(ingredient.quantity).toString()
-              : ''
+            ingredient.quantity ? fracty(ingredient.quantity).toString() : ''
           }</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ingredient.unit}</span>
